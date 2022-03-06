3bcec87bc620bcfc5443a97f4d88fb1c0bbd6e78
FrozenTrojan <funnygum977@gmail.com>
2022-03-06 15:30:13 -0800

    Update

--- Project1.cpp
+++ Project1.cpp
@@ -1,7 +1,8 @@
 // ============================================================================
-// Programmer: 
+// Programmer: , , Ethan Ton
 // Date: 03/11/2022
 // Class: CPSC 232
+// Project: Project 1 Lexer
 // ============================================================================
 #include <iostream>
 #include <vector>
@@ -13,25 +14,68 @@ using namespace std;
 // These are the inputs for the FSM.
 //FOR SCIENCE
 enum TransitionStates {
-    REJECT = 0, // REJECT is the starting state and 
-    INTEGER = 1,
-    REAL = 2,
-    OPERATOR = 3,
-    STRING = 4,
-    SPACE = 5,
-    SEPARATOR = 6,
-    COMMENT = 7,
-    UNKNOWN = 8,
+    // REJECT is the starting state and 
+    REJECT      = 0, 
+    INTEGER     = 1,
+    REAL        = 2,
+    OPERATOR    = 3,
+    STRING      = 4,
+    SPACE       = 5,
+    SEPARATOR   = 6,
+    COMMENT     = 7,
+    UNKNOWN     = 8,
     PLACEHOLDER = 9
 };
 
-// Prototypes
+// ============================================================================
+//  Function Prototypes
+// ============================================================================
 int getCol(char character);
 void lexer(string line);
 
+// ============================================================================
+//  Class Prototypes
+// ============================================================================
 class FSM;
-class StateTable 
+class StateTable;
+class Token;
+// ============================================================================
+//  MAIN
+// ============================================================================
+int main()
 {
+
+    ifstream inFile;
+    int input;
+
+    string file1 = "Input1.txt";
+    string file2 = "Input2.txt";
+    string file3 = "Input3.txt";
+
+    cout << "Please choose which input file to analyze:" << endl;
+    cout << "1: Input File 1";
+    cout << "2: Input File 2";
+    cout << "3: Input File 2";
+
+    cin >> input;
+
+    if(input == 1)
+    {
+
+    }
+    else if(input == 2)
+    {
+        
+    }
+    else
+    {
+
+    }
+
+    return 0;
+}
+
+class StateTable {
 friend FSM; // INPUTS
 protected:  /* INTEGER, REAL, OPERATOR, STRING, SPACE, SEPARATOR, COMMENT, UNKNOWN  */
 int table[9][9] = 
@@ -47,55 +91,32 @@ int table[9][9] =
 };
 
 class Token {
-    //lexer(string) 
-    {
+    //lexer(string) Function
         // NOTE: Keep track of previous state
         // Was the previous line a comment?
-        {
-        }
         // Begin the string walk.
-        {
             // What is the input?
             // get current column/input of current character 
             //getCol(char)
             // get current state of expression
             // State switching mechanism
             // Is current state starting state? (Did you just arrive on a new lexeme?)
-            {
                 // Only add tokens and lexemes that are not SPACE or COMMENT to container.
-                {
-                    
-                }
             // If previous state is COMMENT. We move the expression index forward.
             // Reset lexeme string.
-            }
             // Will skip comments entirely without catching.
             // Catch and concat character for current lexeme
-            {
-
-            }
             // Reassign previous state
-        }
         // Handle end brackets, parethesis, semicolon, etc.
-        if(true)
-        {
-
-        }
         // Handling block comments that extend onto other lines.
-        else if(true)
-        {
 
-        }
-        return tokens;
-    }
-}
+};
 
-int main()
-{
-
-    return 0;
-}
-
+// ============================================================================
+//  int getCol
+//      INPUT - char
+//      OUTPUT - Column number associated with the current input character
+// ============================================================================
 int getCol(char character)
 {
     return 0;
