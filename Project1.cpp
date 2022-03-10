ab3ab379d5a516a51378fcac9de694d3ff74253f
Gregory <85207059+gpytak@users.noreply.github.com>
2022-03-10 13:57:41 -0800

    Update Project1.cpp

--- Project1.cpp
+++ Project1.cpp
@@ -1,5 +1,5 @@
 // ============================================================================
-// Programmer: , , Ethan Ton
+// Programmer: Vincent Hsu, Gregory Pytak, Ethan Ton
 // Date: 03/11/2022
 // Class: CPSC 232
 // Project: Project 1 Lexer
@@ -12,19 +12,18 @@
 
 using namespace std;
 // These are the inputs for the FSM.
-//FOR SCIENCE
 enum TransitionStates {
-    // REJECT is the starting state and 
-    REJECT      = 0, 
-    INTEGER     = 1,
-    REAL        = 2,
-    OPERATOR    = 3,
-    STRING      = 4,
-    SPACE       = 5,
-    SEPARATOR   = 6,
-    COMMENT     = 7,
-    UNKNOWN     = 8,
-    PLACEHOLDER = 9
+	// REJECT is the starting state and final state.
+	REJECT = 0,
+	INTEGER = 1,
+	REAL = 2,
+	OPERATOR = 3,
+	STRING = 4,
+	SPACE = 5,
+	SEPARATOR = 6,
+	COMMENT = 7,
+	UNKNOWN = 8,
+	PLACEHOLDER = 9
 };
 
 // ============================================================================
@@ -46,108 +45,133 @@ class Token;
 int main()
 {
 
-    ifstream inFile;
-    int input;
-    string input = "";
+	ifstream inFile;
+	int input;
+	string fileInput = "";
 
-    string file1 = "Input1.txt";
-    string file2 = "Input2.txt";
-    string file3 = "Input3.txt";
+	string file1 = "Input1.txt";
+	string file2 = "Input2.txt";
+	string file3 = "Input3.txt";
 
-    cout << "Please choose which input file to analyze:" << endl;
-    cout << "1: Input File 1";
-    cout << "2: Input File 2";
-    cout << "3: Input File 2";
+	cout << "Please choose which input file to analyze:" << endl;
+	cout << "1: Input File 1" << endl;
+	cout << "2: Input File 2" << endl;
+	cout << "3: Input File 2" << endl;
 
-    // Receive input from user to select which file to open
-    // Loops if input received is not acceptable
-    while(true)
-    {
-        cin >> input;
-        if(input == 1 || input == 2 || input == 3)
-        {
-            break;
-        }
+	// Receive input from user to select which file to open
+	// Loops if input received is not acceptable
+	while (true)
+	{
+		cin >> input;
+		if (input == 1 || input == 2 || input == 3)
+		{
+			break;
+		}
+		cout << "Please input 1, 2, or 3." << endl;
+	}
 
-        cout << "Please input 1, 2, or 3." << endl;
-    }
+	// Uses user input to select which file to open
+	if (input == 1)
+	{
+		inFile.open(file1);
+	}
+	else if (input == 2)
+	{
+		inFile.open(file2);
+	}
+	else
+	{
+		inFile.open(file3);
+	}
+	if (!inFile.is_open())
+	{
+		cout << "Failed to open file." << endl;
+		return 0;
+	}
 
-    // Uses user input to select which file to open
-    if(input == 1)
-    {
-        inFile.open(file1);
-    }
-    else if(input == 2)
-    {
-        inFile.open(file2);
-    }
-    else
-    {
-        inFile.open(file3);
-    }
 
-    if(!inFile.is_open())
-    {
-        cout << "Failed to open file." << endl;
-        return 0;
-    }
-    
+	while (getline(inFile, fileInput))
+	{
+		lexer(fileInput);
+	}
 
-    while(inFile.peek() != NULL)
-    {
-        
-    }
-
-    inFile.close();
-    return 0;
+	inFile.close();
+	return 0;
 }
 
 // ============================================================================
 //  CLASS StateTable
 // ============================================================================
-class StateTable 
+class StateTable
 {
-friend FSM; // INPUTS
-protected:  /* INTEGER, REAL, OPERATOR, STRING, SPACE, SEPARATOR, COMMENT, UNKNOWN  */
-int table[9][9] = 
-             {{REJECT,    INTEGER,       REAL,          OPERATOR,      STRING,       SPACE,         SEPARATOR,    COMMENT,  UNKNOWN},
-/* STATE 1 */ {INTEGER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 2 */ {REAL,      PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 3 */ {OPERATOR,  PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 4 */ {STRING,    PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 5 */ {SPACE,     PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 6 */ {SEPARATOR, PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
-/* STATE 7 */ {COMMENT,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  REJECT,   PLACEHOLDER  },
-/* STATE 8 */ {UNKNOWN,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER }};
+	friend FSM; // INPUTS
+	protected:  /* INTEGER, REAL, OPERATOR, STRING, SPACE, SEPARATOR, COMMENT, UNKNOWN  */
+	int table[9][9] =
+	{ {REJECT,    INTEGER,       REAL,          OPERATOR,      STRING,       SPACE,         SEPARATOR,    COMMENT,  UNKNOWN},
+		/* STATE 1 */ {INTEGER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 2 */ {REAL,      PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 3 */ {OPERATOR,  PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 4 */ {STRING,    PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 5 */ {SPACE,     PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 6 */ {SEPARATOR, PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER  },
+		/* STATE 7 */ {COMMENT,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  REJECT,   PLACEHOLDER  },
+		/* STATE 8 */ {UNKNOWN,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,   PLACEHOLDER,  PLACEHOLDER,   PLACEHOLDER,  COMMENT,  PLACEHOLDER } };
 };
 
 // ============================================================================
 //  CLASS Token
 // ============================================================================
-class Token 
+class Token
 {
-    string token;
-    int lexeme;
-    string lexemeLiteral;
+	void lexer(string fileInput)
+	{
+		string token = "";
+		int lexeme;
+		int col = REJECT;
+		int currentState = REJECT;
+		int previousState = REJECT;
+		char currentChar = ' ';
+		string lexemeLiteral;
 
-    //lexer(string) Function
-        // NOTE: Keep track of previous state
-        // Was the previous line a comment?
-        // Begin the string walk.
-            // What is the input?
-            // get current column/input of current character 
-            //getCol(char)
-            // get current state of expression
-            // State switching mechanism
-            // Is current state starting state? (Did you just arrive on a new lexeme?)
-                // Only add tokens and lexemes that are not SPACE or COMMENT to container.
-            // If previous state is COMMENT. We move the expression index forward.
-            // Reset lexeme string.
-            // Will skip comments entirely without catching.
-            // Catch and concat character for current lexeme
-            // Reassign previous state
-        // Handle end brackets, parethesis, semicolon, etc.
-        // Handling block comments that extend onto other lines.
+		for (int i = 0; i < fileInput.length(); i++)
+		{
+			currentChar = fileInput[i];
+			col = getCol(currentChar);
+			currentState = table[currentState][col];
+			
+			if (currentState == REJECT)
+			{
+				if (previousState != SPACE)
+				{
+
+				}
+				token = "";
+			}
+			else
+			{
+				token += currentChar;
+			}
+			previousState = currentState;
+		}
+	}
+	//lexer(string) Function
+		// NOTE: Keep track of previous state
+		// Was the previous line a comment?
+		// Begin the string walk.
+			// What is the input?
+			// get current column/input of current character 
+			//getCol(char)
+			// get current state of expression
+			// State switching mechanism
+			// Is current state starting state? (Did you just arrive on a new lexeme?)
+				// Only add tokens and lexemes that are not SPACE or COMMENT to container.
+			// If previous state is COMMENT. We move the expression index forward.
+			// Reset lexeme string.
+			// Will skip comments entirely without catching.
+			// Catch and concat character for current lexeme
+			// Reassign previous state
+		// Handle end brackets, parethesis, semicolon, etc.
+		// Handling block comments that extend onto other lines.
 
 };
 
@@ -158,7 +182,7 @@ class Token
 // ============================================================================
 int getCol(char character)
 {
-    return 0;
+	return 0;
 }
 
 // ============================================================================
